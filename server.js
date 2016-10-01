fae96bee42607be4e3c320a4f8ede5585f27e197
variarp <induprdp@gmail.com>
2016-10-01 12:45:18 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function createTemplate(data){
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
     
-        <div id="main-text" class="container">
+        <div class="container">
             <div>
                 <a href="/">Home</a>
             </div>
