d296a2d011dd7b5041fcd652cfd09042537898a9
variarp <induprdp@gmail.com>
2016-10-01 12:59:07 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,10 @@ console.log('Loaded!');
 //manipulate html content
 
 var mainText = document.getElementById('main-text');
-//mainText.innerHTML='New from Source';
+mainText.innerHTML='New from Source';
+
+//Move image
+var img = document.getElementById('madi');
+img.onClick=function(){
+   img.style.marginLeft="100px";
+}
