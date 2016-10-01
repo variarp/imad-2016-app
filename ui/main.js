701561c75ba0cd2c554592959a916fb5fae50486
variarp <induprdp@gmail.com>
2016-10-01 13:14:57 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ mainText.innerHTML='New from Source';
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=10;
+    marginLeft+=1;
     img.style.marginLeft=marginLeft+"px";
 }
 img.onclick=function(){
-   var interval = setInterval(moveRight,100);
+   var interval = setInterval(moveRight,50);
 };
