2b867b211062401b761d728c6bde9886a9801eb6
Pradeep Mohan <Pradeep Mohan>
2016-10-14 21:24:12 +0400

    changes EOD 14Oct16

--- server.js
+++ server.js
@@ -57,6 +57,44 @@ var articles={
 }
 };
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter=0;
+app.get('/counter', function (req, res){
+ counter+=1;
+ res.send(counter.toString());
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var names=[];
+app.get('/submit-name', function(req,res){
+    //get name
+    var name=req.query.name;
+    //add to string array
+    names.push(name);
+    //send in response
+    res.send(JSON.stringify(names));
+
+});
+
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
 function createTemplate(data){
     
     var article=data.article;
@@ -94,27 +132,6 @@ function createTemplate(data){
     return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,14 @@
 console.log('Loaded!');
+var serverPath='localhost:8080'; 
+//var serverPath='variarp.imad.hasura-app.io'; //hasura site
 //manipulate html content
 
-var mainText = document.getElementById('main-text');
-mainText.innerHTML='New from Source';
+//var mainText = document.getElementById('main-text');
+//mainText.innerHTML='New from Source';
 
 //Move image
+//clicking on image speeds up the movement. why?
+//setInterval(moveRight,50) what does this do exactly
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
@@ -13,4 +17,53 @@ function moveRight(){
 }
 img.onclick=function(){
    var interval = setInterval(moveRight,50);
+};
+
+//counter code
+var button=document.getElementById('Counter');
+var counter=0;
+button.onclick = function(){
+
+	//create request object
+	var request = new XMLHttpRequest();
+
+	//read the response
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+			if (request.status === 200){
+				var counter=request.responseText;
+				//update span 'count' with counter value
+				var span=document.getElementById('count');
+				span.innerHTML=(counter).toString();
+			}
+		}
+		//else do nothing. keep waiting.
+	};
+
+	request.open('GET','http://'+serverPath+'/counter',true);
+	request.send(null);
+};
+
+var submit=document.getElementById("submit_btn");
+submit.onclick = function () {
+	
+	//create a request object
+	var request = new XMLHttpRequest;
+	//read the response and render the response into HTML
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+			if (request.status === 200){
+			var names=JSON.parse(request.responseText);
+			var list= '';
+			for (var i=0;i<names.length;i++){
+					list+='<li>'+names[i]+'</li>';
+				}
+			document.getElementById("namelist").innerHTML=list;	
+			}
+		}
+	}
+	//make a request
+	var name=document.getElementById("name").value;
+	request.open('GET','http://'+serverPath+'/submit-name?name='+name,true);
+	request.send(null);
 };
